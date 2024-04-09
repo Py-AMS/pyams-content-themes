e7dfa9e77075b849681021fbdf17bd6150c40688
Thierry Florac <thierry.florac@onf.fr>
2024-04-09 23:01:45 +0200

    Version 1.99.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ DOCS = os.path.join(os.path.dirname(__file__),
 README = os.path.join(DOCS, 'README.rst')
 HISTORY = os.path.join(DOCS, 'HISTORY.rst')
 
-version = '1.99.0'
+version = '1.99.1'
 long_description = open(README).read() + '\n\n' + open(HISTORY).read()
 
 data_dir = 'pkg'
