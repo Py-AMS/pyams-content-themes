d9944c44db61a489c0f047721b85f942a6493e95
Thierry Florac <tflorac@ulthar.net>
2024-06-24 23:15:49 +0200

    Added filters management

--- src/pyams_content_themes/resources/src/js/_search.js
+++ src/pyams_content_themes/resources/src/js/_search.js
@@ -21,6 +21,25 @@ const PyAMS_search = {
 		}
 	},
 
+	updateSearchFilters: (evt) => {
+		const
+			urlParams = new URLSearchParams(window.location.search),
+			filterTypes = new Set($('input[name="filter"], select[name="filter"]').map((idx, elt) => {
+				return $(elt).data('filter-type');
+			}).get());
+		filterTypes.forEach(type => urlParams.delete(type));
+		$('input[name="filter"]:checked, select[name="filter"]').each((idx, elt) => {
+			const filterType = $(elt).data('filter-type');
+			const value = $(elt).val();
+			// Append each selected filter value under its respective filter type
+			if (value) {
+				urlParams.append(filterType, value);
+			}
+		});
+		// Construct the new URL with updated parameters
+		window.location.href = `${window.location.protocol}//${window.location.host}${window.location.pathname}?${urlParams.toString()}`;
+	},
+
 	previousPage: (evt) => {
 		const form = $('form[id="search-results"]');
 		if (form.length > 0) {
@@ -49,6 +68,32 @@ const PyAMS_search = {
 			$('input[name="start"]', form).val(length * (target - 1));
 			form.submit();
 		}
+	},
+
+	switchFilter: (evt) => {
+		const
+			target = $(evt.currentTarget),
+			header = $(`[data-toggle="collapse"][href="#${target.attr('id')}"]`);
+		$('.fa', header).toggleClass('fa-caret-down').toggleClass('fa-caret-up');
+	},
+
+	switchMoreFilters: (evt) => {
+		const
+			target = $(evt.currentTarget),
+			switcher = $(`[data-toggle="collapse"][data-target="#${target.attr('id')}"]`);
+		if (target.hasClass('show')) {
+			switcher.text(switcher.data('ams-less-label'))
+		} else {
+			switcher.text(switcher.data('ams-more-label'));
+		}
+	},
+
+	resetFilters: (evt) => {
+		const
+			form = $(evt.currentTarget).parents('form');
+		$('input[type="checkbox"]', form).prop('checked', false);
+		$('select', form).prop('selectedIndex', 0);
+		PyAMS_search.updateSearchFilters(evt);
 	}
 };
 
